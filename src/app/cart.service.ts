c986f7ac9ce02092e3ab68cdc0f6ba19a9f2dfb6
MasatakaIshii <masaradio78@gmail.com>
2022-10-25 16:03:00 +0200

    change

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
+import {environment} from "../environments/environment"
 
 @Injectable({
   providedIn: 'root',
@@ -23,7 +24,7 @@ export class CartService {
   }
 
   getTodo() {
-    return this.http.get<any>('http://localhost:8080/api/todo');
+    return this.http.get<any>(environment.javaApi + '/todo');
   }
 
   getShippingPrices() {
